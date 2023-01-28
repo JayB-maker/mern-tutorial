09e5d304d3eb21b3081876214f2013ed239c7ce9
Abiodun Jayb <ajiboyeabiodun001@gmail.com>
2023-01-28 11:52:51 +0100

    database setups and connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const colors = require('colors')
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/user", require("./routes/userRoute"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`server started on port ${port}`));
